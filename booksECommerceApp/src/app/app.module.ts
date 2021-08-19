9a65569158b4ca744e980b17796f74e211a9f903
Aida Perez <aida.capeca@gmail.com>
2021-08-19 19:07:01 +0200

    * Integration of components into app.module

--- booksECommerceApp/src/app/app.module.ts
+++ booksECommerceApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component/header.component';
+import { ProductCardComponent } from './components/product-card.component/product-card.component';
+import { CheckoutDashboardComponent } from './screens/checkout-dashboard/checkout-dashboard.component';
+import { ProductDashboardComponent } from './screens/product-dashboard/product-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductCardComponent,
+    CheckoutDashboardComponent,
+    ProductDashboardComponent
   ],
   imports: [
     BrowserModule,
