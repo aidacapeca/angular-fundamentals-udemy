4f9803ad88cc37635b8521bab8413cec8bd325a5
Aida Perez <aida.capeca@gmail.com>
2021-09-14 19:29:43 +0200

    Include functionality on productDashboard component integrating modal and handle events

--- booksECommerceApp/src/app/screens/product-dashboard/product-dashboard.component.ts
+++ booksECommerceApp/src/app/screens/product-dashboard/product-dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { SingleBook } from 'src/app/interfaces/ecommerce.interface';
+import {MatDialog} from '@angular/material/dialog';
 
+import {ECOMMERCE_BOOKS} from '../../constants/main-data'
+import { ConfirmationModalComponent } from 'src/app/components/confirmation-modal/confirmation-modal.component';
 @Component({
   selector: 'app-product-dashboard',
   templateUrl: './product-dashboard.component.html',
   styleUrls: ['./product-dashboard.component.scss']
 })
 export class ProductDashboardComponent implements OnInit {
-
-  constructor() { }
+  public productsInventary = ECOMMERCE_BOOKS;
+  public shoppingCart = [] as SingleBook[]
+  
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+  }
+
+  private _addProductToCart(bookSelected: SingleBook): void {
+    this.shoppingCart.push(bookSelected);
+    console.log(this.shoppingCart);
+  }
+
+  public openAddProductModal(bookSelected: SingleBook): void {
+    this._addProductToCart(bookSelected);
+    const dialogRef = this.dialog.open(ConfirmationModalComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
